7f118d902ab2b6b52470992250ce5a1ac8cd9d86
Prem <prem89k@gmail.com>
2016-03-23 17:06:16 +0530

    Add Migration and base ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
+//= require nprogress
+//= require nprogress-turbolinks
+//= require bootstrap
 //= require_tree .
