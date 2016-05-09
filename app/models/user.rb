32cdd99c2b8f80e542c4ac4b97b2f62614370e26
madanrules <madan.snist@gmail.com>
2016-05-09 17:02:15 +0530

    Rate and Review

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   has_many :orders
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  ratyrate_rater
 end
